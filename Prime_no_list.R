db1fa7a37584ec1c7b3fa52a6c05d29026314fd6
gauravdakliya <gauravdakliya1012@gmail.com>
2017-05-28 23:45:54 +0530

    Add files via upload
    Most efficient way to find the list of all prime no. before a given no.

---
+++ Prime_no_list.R
@@ -0,0 +1,16 @@
+prime = function(a,n){
+  n=c(2)
+  i=3
+  while(i <=a){
+    for(j in n){
+      r=0
+      if (i%%j == 0){
+        r=1}
+      if(r==1){break}
+      
+    }
+    if(r!=1){n = c(n,i)}
+    i=i+2
+  }
+  print(n)
+}
